f927bbd7833219840075aa72bc6fa38021d6e548
Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>
2020-04-11 16:54:42 +0200

    Create server base
    Signed-off-by: Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type task struct {
+	ID       int    `json:"id,omitempty"`
+	Title    string `json:"title,omitempty"`
+	Finished bool   `json:"finished,omitempty"`
+}
+
+var taskList []task
+
+func getTaskEndPoint(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusFound)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(taskList)
+}
 
 func main() {
-	fmt.Println("Hola")
+	router := mux.NewRouter()
+	taskList = []task{}
+
+	router.HandleFunc("/task", getTaskEndPoint)
+
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
