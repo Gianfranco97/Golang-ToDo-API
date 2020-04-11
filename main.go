780e119a53b85d8824ea48c39174ec4003b4b1a0
Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>
2020-04-11 18:16:16 +0200

    Delete a task
    Signed-off-by: Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>

--- main.go
+++ main.go
@@ -75,13 +75,41 @@ func getOneTaskEndPoint(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(task{})
 }
 
+func deleteOneTaskEndPoint(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errorMessage{Message: "The requested ID is not valid"})
+		return
+	}
+
+	for index, item := range taskList {
+		if item.ID == id {
+			taskList = append(taskList[:index], taskList[index+1:]...)
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(task{})
+			return
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNotFound)
+	json.NewEncoder(w).Encode(task{})
+}
+
 func main() {
 	router := mux.NewRouter()
 	taskList = []task{}
 
-	router.HandleFunc("/task", getTaskEndPoint).Methods("Get")
-	router.HandleFunc("/task", addOneTaskEndPoint).Methods("Post")
-	router.HandleFunc("/task/{id}", getOneTaskEndPoint).Methods("Get")
+	router.HandleFunc("/task", getTaskEndPoint).Methods("GET")
+	router.HandleFunc("/task", addOneTaskEndPoint).Methods("POST")
+	router.HandleFunc("/task/{id}", getOneTaskEndPoint).Methods("GET")
+	router.HandleFunc("/task/{id}", deleteOneTaskEndPoint).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
