d0fd5cfdf8d1d113602b457761fc67cafd848f35
Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>
2020-04-11 17:46:44 +0200

    Add a new task
    Signed-off-by: Gianfranco Manganiello <gianfrancomanganiello1997@gmail.com>

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -22,9 +23,31 @@ type errorMessage struct {
 var taskList []task
 
 func getTaskEndPoint(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(taskList)
+}
+
+func addOneTaskEndPoint(w http.ResponseWriter, r *http.Request) {
+	var newTask task
+	reqBody, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(errorMessage{Message: "The task data is not valid"})
+		return
+	}
+
+	json.Unmarshal(reqBody, &newTask)
+	newTask.ID = len(taskList) + 1
+
+	taskList = append(taskList, newTask)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+
 }
 
 func getOneTaskEndPoint(w http.ResponseWriter, r *http.Request) {
@@ -32,23 +55,23 @@ func getOneTaskEndPoint(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(params["id"])
 
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
 		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(errorMessage{Message: "The requested ID is not valid"})
 		return
 	}
 
 	for _, item := range taskList {
 		if item.ID == id {
-			w.WriteHeader(http.StatusOK)
 			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
 			json.NewEncoder(w).Encode(item)
 			return
 		}
 	}
 
-	w.WriteHeader(http.StatusNotFound)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNotFound)
 	json.NewEncoder(w).Encode(task{})
 }
 
@@ -56,8 +79,9 @@ func main() {
 	router := mux.NewRouter()
 	taskList = []task{}
 
-	router.HandleFunc("/task", getTaskEndPoint)
-	router.HandleFunc("/task/{id}", getOneTaskEndPoint)
+	router.HandleFunc("/task", getTaskEndPoint).Methods("Get")
+	router.HandleFunc("/task", addOneTaskEndPoint).Methods("Post")
+	router.HandleFunc("/task/{id}", getOneTaskEndPoint).Methods("Get")
 
 	log.Fatal(http.ListenAndServe(":3000", router))
 }
